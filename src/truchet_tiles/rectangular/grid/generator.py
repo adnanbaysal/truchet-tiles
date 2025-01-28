1850b70e1e5d2a56dd4d4dacdfbecbf43b0cb997
Adnan Baysal <adbaysal@gmail.com>
2025-01-28 22:52:10 +0300

    Complete rectangular grids

--- src/truchet_tiles/rectangular/grid/generator.py
+++ src/truchet_tiles/rectangular/grid/generator.py
@@ -4,6 +4,15 @@ from functools import cache
 from random import randint
 
 from truchet_tiles.common.math import parity
+from truchet_tiles.common.number_triangle import (
+    get_baysal_triangle,
+    get_hosoya_triangle,
+    get_pascal_triangle,
+)
+from truchet_tiles.rectangular.grid.triangle_converter import (
+    triangle_to_reflected_square,
+    triangle_to_subsquare,
+)
 
 
 class RectGridType(str, Enum):
@@ -19,6 +28,48 @@ class RectGridType(str, Enum):
     THUESHIFT = "thueshift"
     ZEROS = "zeros"
     ONES = "ones"
+    PASCALSUBXOR = "pascalsubxor"
+    PASCALSUBMOD = "pascalsubmod"
+    PASCALREFXOR = "pascalrefxor"
+    PASCALREFMOD = "pascalrefmod"
+    BAYSALSUBXOR = "baysalsubxor"
+    BAYSALSUBMOD = "baysalsubmod"
+    BAYSALREFXOR = "baysalrefxor"
+    BAYSALREFMOD = "baysalrefmod"
+    HOSOYASUBXOR = "hosoyasubxor"
+    HOSOYASUBMOD = "hosoyasubmod"
+    HOSOYAREFXOR = "hosoyarefxor"
+    HOSOYAREFMOD = "hosoyarefmod"
+
+
+@cache
+def baysal_reflected_square(grid_size: int) -> defaultdict[tuple[int, int], int]:
+    return triangle_to_reflected_square(get_baysal_triangle(grid_size))
+
+
+@cache
+def baysal_subsquare(grid_size: int) -> defaultdict[tuple[int, int], int]:
+    return triangle_to_subsquare(get_baysal_triangle(2 * grid_size))
+
+
+@cache
+def hosoya_reflected_square(grid_size: int) -> defaultdict[tuple[int, int], int]:
+    return triangle_to_reflected_square(get_hosoya_triangle(grid_size))
+
+
+@cache
+def hosoya_subsquare(grid_size: int) -> defaultdict[tuple[int, int], int]:
+    return triangle_to_subsquare(get_hosoya_triangle(2 * grid_size))
+
+
+@cache
+def pascal_reflected_square(grid_size: int) -> defaultdict[tuple[int, int], int]:
+    return triangle_to_reflected_square(get_pascal_triangle(grid_size))
+
+
+@cache
+def pascal_subsquare(grid_size: int) -> defaultdict[tuple[int, int], int]:
+    return triangle_to_subsquare(get_pascal_triangle(2 * grid_size))
 
 
 @cache
@@ -48,6 +99,30 @@ def get_rect_grid(
             grid_func = lambda x, y: 0  # noqa: E731
         case RectGridType.ONES:
             grid_func = lambda x, y: 1  # noqa: E731
+        case RectGridType.PASCALSUBXOR:
+            grid_func = lambda x, y: parity(pascal_subsquare(grid_size)[(x, y)])  # noqa: E731
+        case RectGridType.PASCALSUBMOD:
+            grid_func = lambda x, y: pascal_subsquare(grid_size)[(x, y)] % 2  # noqa: E731
+        case RectGridType.PASCALREFXOR:
+            grid_func = lambda x, y: parity(pascal_reflected_square(grid_size)[(x, y)])  # noqa: E731
+        case RectGridType.PASCALREFMOD:
+            grid_func = lambda x, y: pascal_reflected_square(grid_size)[(x, y)] % 2  # noqa: E731
+        case RectGridType.BAYSALSUBXOR:
+            grid_func = lambda x, y: parity(baysal_subsquare(grid_size)[(x, y)])  # noqa: E731
+        case RectGridType.BAYSALSUBMOD:
+            grid_func = lambda x, y: baysal_subsquare(grid_size)[(x, y)] % 2  # noqa: E731
+        case RectGridType.BAYSALREFXOR:
+            grid_func = lambda x, y: parity(baysal_reflected_square(grid_size)[(x, y)])  # noqa: E731
+        case RectGridType.BAYSALREFMOD:
+            grid_func = lambda x, y: baysal_reflected_square(grid_size)[(x, y)] % 2  # noqa: E731
+        case RectGridType.HOSOYASUBXOR:
+            grid_func = lambda x, y: parity(hosoya_subsquare(grid_size)[(x, y)])  # noqa: E731
+        case RectGridType.HOSOYASUBMOD:
+            grid_func = lambda x, y: hosoya_subsquare(grid_size)[(x, y)] % 2  # noqa: E731
+        case RectGridType.HOSOYAREFXOR:
+            grid_func = lambda x, y: parity(hosoya_reflected_square(grid_size)[(x, y)])  # noqa: E731
+        case RectGridType.HOSOYAREFMOD:
+            grid_func = lambda x, y: hosoya_reflected_square(grid_size)[(x, y)] % 2  # noqa: E731
         case _:
             grid_func = lambda x, y: randint(0, 1)  # noqa: E731
 
